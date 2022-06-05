2db4a800fb85a88d85e8a86d4fbed13542ee58fc
czarnaherbata <czarnaherbata223@gmail.com>
2022-06-05 14:19:19 +0200

    dodanie walidacji na rejestrację
    niestety nie pokazuje się nwm dlaczego

--- src/main/java/com/example/wwwjava/controllers/LoginController.java
+++ src/main/java/com/example/wwwjava/controllers/LoginController.java
@@ -3,16 +3,26 @@ package com.example.wwwjava.controllers;
 import com.example.wwwjava.models.User;
 import com.example.wwwjava.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
+
+@InitBinder
+    private void initBinder(WebDataBinder dataBinder)
+    {
+        StringTrimmerEditor stringTrimmerEditor=new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
     @Autowired
     private UserDao userService;
     @Autowired private BCryptPasswordEncoder providedEncodere;
--- src/main/java/com/example/wwwjava/models/User.java
+++ src/main/java/com/example/wwwjava/models/User.java
@@ -2,9 +2,13 @@ package com.example.wwwjava.models;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -19,18 +23,26 @@ public class User {
 
     @Column(nullable = false, unique = true, length = 45)
     @NotEmpty
+    @NotBlank(message = "Wpisz swój email")
+    @Email(message="Wpisz poprawny email")
+    @Size(min=5, max=50)
     private String email;
 
     @Column(nullable = false, length = 64)
     @NotEmpty
+    @NotBlank(message = "Wpisz swoje hasło")
+    @Length(min=6, message = "Hasło musi zawierać co najmniej 6 znaków")
     private String password;
 
     @Column(name = "first_name", nullable = false, length = 20)
     @NotEmpty
+    @NotBlank(message = "Wprowadź imie")
     private String firstName;
 
     @Column(name = "last_name", nullable = false, length = 20)
     @NotEmpty
+    @NotBlank(message = "Wprowadź nazwisko")
+
     private String lastName;
 
     @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
